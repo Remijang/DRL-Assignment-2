25bac825c568bcce079caa30a9aa76ce5e62b49c
Remijang <wuet0829@gmail.com>
2025-04-10 20:01:50 +0800

    test

--- student_agent.py
+++ student_agent.py
@@ -156,6 +156,27 @@ class Game2048Env(gym.Env):
 
         return self.board, self.score, done, {}
 
+    def try_step(self, action):
+        """Execute one action"""
+        assert self.action_space.contains(action), "Invalid action"
+
+        if action == 0:
+            moved = self.move_up()
+        elif action == 1:
+            moved = self.move_down()
+        elif action == 2:
+            moved = self.move_left()
+        elif action == 3:
+            moved = self.move_right()
+        else:
+            moved = False
+
+        self.last_move_valid = moved  # Record if the move was valid
+
+        done = self.is_game_over()
+
+        return self.board, self.score, done, {}
+
     def render(self, mode="human", action=None):
         """
         Render the current board using Matplotlib.
@@ -231,10 +252,142 @@ class Game2048Env(gym.Env):
         # If the simulated board is different from the current board, the move is legal
         return not np.array_equal(self.board, temp_board)
 
+
+
+
+def rot90(board):
+    return [(j, 3 - i) for i, j in board]
+def rot180(board):
+    return [(3 - i, 3 - j) for i, j in board]
+def rot270(board):
+    return [(3 - j, i) for i, j in board]
+def flip_vertical(board):
+    return [(3 - i, j) for i, j in board]
+def flip_horizontal(board):
+    return [(i, 3 - j) for i, j in board]
+
+
+class NTupleApproximator:
+    def __init__(self, board_size, patterns):
+        self.board_size = board_size
+        self.patterns = patterns
+        self.weights = [[i for i in range(10)] for _ in range(len(self.patterns))]
+        self.symmetry_patterns = []
+        for pattern in self.patterns:
+            syms = self.generate_symmetries(pattern)
+            for syms_ in syms:
+                self.symmetry_patterns.append(syms_)
+        self.initial_value = 320000 / 64
+
+    def generate_symmetries(self, pattern):
+        return [
+            pattern,
+            rot90(pattern),
+            rot180(pattern),
+            rot270(pattern),
+            flip_vertical(pattern),
+            flip_horizontal(pattern),
+            flip_vertical(rot90(pattern)),
+            flip_horizontal(rot90(pattern)),
+        ]
+
+    def tile_to_index(self, tile):
+        if tile == 0:
+            return 0
+        else:
+            return int(math.log(tile, 2))
+
+    def get_feature(self, board, coords):
+        idx = 0
+        for i, coord in enumerate(coords):
+            idx *= 16
+            idx += self.tile_to_index(board[coord[0], coord[1]])
+        return idx
+
+    def value(self, board):
+        value = 0.0
+        for i, pattern in enumerate(self.symmetry_patterns):
+            feature = self.get_feature(board, pattern)
+            value += self.weights[i // 8][feature]
+        return value
+
+
+def expectimax_search(env, approximator, depth, root):
+    legal_moves = [a for a in range(4) if env.is_move_legal(a)]
+    if depth == 0 or not legal_moves:
+        return None, approximator.value(env.board)
+
+    best_action = None
+    best_value = -float('inf')
+    old_score = env.score
+    score_list = [0] * 4
+    for action in legal_moves:
+        env_copy = copy.deepcopy(env)
+        _, new_score, _, _ = env_copy.try_step(action)
+        reward = new_score - old_score
+        expected_value = expect_chance_nodes(env_copy, approximator, depth - 1) + reward
+        if expected_value > best_value:
+            best_value = expected_value
+            best_action = action
+        score_list[action] = expected_value
+    print(score_list)
+    return best_action, best_value
+
+def expect_chance_nodes(env, approximator, depth):
+    if depth == 0:
+        return approximator.value(env.board)
+    possible_tiles = [(i, j) for i in range(4) for j in range(4) if env.board[i, j] == 0]
+    expected_value = 0
+    num_possible_tiles = len(possible_tiles)
+    if num_possible_tiles == 0:
+        return expectimax_search(env, approximator, depth, 0)[1]
+    for i, j in possible_tiles:
+        for tile_value, probability in [(2, 0.9), (4, 0.1)]:
+            env_copy = copy.deepcopy(env)
+            env_copy.board[i, j] = tile_value
+            expected_value += probability * expectimax_search(env_copy, approximator, depth - 1, 0)[1]
+    return expected_value / num_possible_tiles
+
+
+patterns = [
+    [(0, 0), (0, 1), (0, 2), (0, 3), (1, 0), (1, 1)],
+    [(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2)],
+    [(0, 1), (0, 2), (1, 1), (1, 2), (2, 1), (3, 1)],
+    [(0, 0), (0, 1), (1, 1), (1, 2), (1, 3), (2, 2)],
+    [(0, 0), (0, 1), (0, 2), (1, 1), (2, 1), (2, 2)],
+    [(0, 0), (0, 1), (1, 1), (2, 1), (3, 1), (3, 2)],
+    [(0, 0), (0, 1), (1, 1), (2, 0), (2, 1), (3, 1)],
+    [(0, 0), (0, 1), (0, 2), (1, 0), (1, 2), (2, 2)]
+]
+
+import struct
+
+def load_large_vector_from_binary_file(filename):
+    try:
+        with open(filename, 'rb') as infile:
+            # 讀取維度資訊
+            dim2 = struct.unpack('<Q', infile.read(8))[0]
+            dim3 = struct.unpack('<Q', infile.read(8))[0]
+
+            matrix = []
+            for j in range(dim2):
+                row_bytes = infile.read(dim3 * 4)
+                row = list(struct.unpack('<{}f'.format(dim3), row_bytes))
+                matrix.append(row)
+        return matrix
+
+    except FileNotFoundError:
+        print(f"Error opening file: {filename}")
+    except struct.error:
+        print("Error reading file format.")
+
+
+approximator = NTupleApproximator(board_size = 4, patterns = patterns)
+approximator.weights = load_large_vector_from_binary_file("approximator")
+
+
 def get_action(state, score):
     env = Game2048Env()
-    return random.choice([0, 1, 2, 3]) # Choose a random action
-    
-    # You can submit this random agent to evaluate the performance of a purely random strategy.
-
-
+    env.board = copy.deepcopy(state)
+    best_action, _ = expectimax_search(env, approximator, depth = 1, root = 1)
+    return best_action
